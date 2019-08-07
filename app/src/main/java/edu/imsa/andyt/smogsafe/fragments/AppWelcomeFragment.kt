306769bee92839d2bd6d53d32feb59fb472168b3
Andy Tang <winterwind2022@gmail.com>
2019-08-07 09:12:30 -0500

    Detailed debugging of the merge

--- app/src/main/java/edu/imsa/andyt/smogsafe/fragments/AppWelcomeFragment.kt
+++ app/src/main/java/edu/imsa/andyt/smogsafe/fragments/AppWelcomeFragment.kt
@@ -1,21 +1,13 @@
 package edu.imsa.andyt.smogsafe.fragments
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
 import edu.imsa.andyt.smogsafe.R
 import kotlinx.android.synthetic.main.fragment_app_welcome.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -30,9 +22,6 @@ class AppWelcomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        buttonStart.setOnClickListener {
-            findNavController().navigate(R.id.action_appWelcomeFragment_to_locationListFragment)
-        }
     }
 
     override fun onCreateView(
@@ -43,5 +32,11 @@ class AppWelcomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_app_welcome, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        buttonStart.setOnClickListener {
+            findNavController().navigate(R.id.action_appWelcomeFragment_to_locationListFragment)
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
+
+}
