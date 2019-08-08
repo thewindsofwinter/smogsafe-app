f7435ee5301e32a78c2e0cccf6c279989f8b59c1
Andy Tang <winterwind2022@gmail.com>
2019-08-08 11:38:03 -0500

    commit, whoops wrong branch

--- app/src/main/java/edu/imsa/andyt/smogsafe/fragments/LocationDetailFragment.kt
+++ app/src/main/java/edu/imsa/andyt/smogsafe/fragments/LocationDetailFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import edu.imsa.andyt.smogsafe.R
+import edu.imsa.andyt.smogsafe.database.MyViewModel
+import edu.imsa.andyt.smogsafe.models.LocationWrapper
+import kotlinx.android.synthetic.main.fragment_location_detail.*
 
 
 /**
@@ -19,6 +24,8 @@ import edu.imsa.andyt.smogsafe.R
  */
 class LocationDetailFragment : Fragment() {
 
+    val viewModel: MyViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,4 +34,38 @@ class LocationDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_location_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var position: Int = 0;
+        arguments?.let {
+            position = it.getInt("POSITION")
+        }
+
+        val l: LocationWrapper = viewModel.myNotes[position]
+        cityName?.text = l.data.city.name;
+        cityAirQuality?.text = l.data.aqi.toString();
+        cityLocation?.text = "(${l.data.city.geo[0]}, ${l.data.city.geo[1]})";
+        updateTime?.text = "${l.data.time.s} UTC${l.data.time.tz}";
+
+
+        val map: Map<TextView, Double> = mapOf(
+            Pair(pm25, l.data.iaqi.pm25.v),
+            Pair(pm10, l.data.iaqi.pm10.v),
+            Pair(o3, l.data.iaqi.o3.v),
+            Pair(no2, l.data.iaqi.no2.v),
+            Pair(so2, l.data.iaqi.so2.v),
+            Pair(pm, l.data.iaqi.p.v),
+            Pair(co, l.data.iaqi.co.v),
+            Pair(hc, l.data.iaqi.h.v),
+            Pair(w, l.data.iaqi.w.v)
+        )
+        for (i in map) {
+            if (i.value == -1.0) {
+                i.key.text = "N/A"
+            } else {
+                i.key.text = i.value.toString()
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
